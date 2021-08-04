b7fa58d26e14d3fc55248a18449bd9b7b73737ed
Pratistha <rinsin2002@gmail.com>
2021-08-04 20:38:13 +0530

    api done
    api done

--- Medics/public/javascripts/pollutionRender.js
+++ Medics/public/javascripts/pollutionRender.js
@@ -1,15 +1,15 @@
-var lat = document.querySelector('.user');
-
+var poll_submit= document.querySelector('.submit');
+var lat = document.querySelector('#latitude');
+var long = document.querySelector('#longitude');
+console.log(lat.value);
 var input1 = document.querySelector('.input1');
 var input2 = document.querySelector('.input2');
 var input3 = document.querySelector('.input3');
 var input4 = document.querySelector('.input4');
 var input5 = document.querySelector('.input5');
-//var input6 = document.querySelector('.input6');
-//const lat=;
-//const long=-73.00597;
 
-fetch("https://air-quality.p.rapidapi.com/current/airquality?lat=40.71427&lon=-73.00597", {
+poll_submit.addEventListener('click', function(){
+    fetch('https://air-quality.p.rapidapi.com/current/airquality?lat='+lat.value+'&lon='+long.value,{
     "method": "GET",
     "headers": {
         "x-rapidapi-key": "ee57f911c9msh54de0a6c51c79a3p153facjsn8e9674e30bb6",
@@ -28,11 +28,12 @@ fetch("https://air-quality.p.rapidapi.com/current/airquality?lat=40.71427&lon=-7
     input1.innerHTML=i1+ " ppm";
     input2.innerHTML=i2+ " ppm";
     input3.innerHTML=i3+ " ppm";
-    input4.innerHTML=i4+"";
-    input5.innerHTML=i5+"";
+    input4.innerHTML=i4;
+    input5.innerHTML=i5;
 
 
 })
 .catch(err => {
     console.error(err);
 });
+})
